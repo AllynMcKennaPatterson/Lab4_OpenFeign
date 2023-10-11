51fe37ce662ccd4fb9f3aab4ddbb0bd26dedf200
G00392765@atu.ie <G00392765@atu.ie>
2023-10-11 09:45:20 +0100

    Initial commit

---
+++ src/main/java/atu/cicd2/lab4_openfeign/Lab4OpenFeignApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4OpenFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4OpenFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atu/cicd2/lab4_openfeign/Lab4OpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4OpenFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
